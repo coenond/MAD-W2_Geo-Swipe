89663f37641dc02217701525cae82183a548f63d
Coen Onderwater <coenonderwater@gmail.com>
2018-09-10 15:16:41 +0200

    feature: add class to store the city

--- app/src/main/java/com/example/coen/mad_w2_geo_swipe/MainActivity.kt
+++ app/src/main/java/com/example/coen/mad_w2_geo_swipe/MainActivity.kt
@@ -5,8 +5,17 @@ import android.os.Bundle
 
 class MainActivity : AppCompatActivity() {
 
+    private var cityImages = ArrayList<CityImage>()
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
     }
+}
+
+class CityImage(isInEurope: Boolean, cityName: String, resourceId: Int) {
+    val inEurope = isInEurope
+    val city = cityName
+    val resource = resourceId
 }
