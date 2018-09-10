9d2ab6caf8b1904137f30057bf778117004a9dcb
Coen Onderwater <coenonderwater@gmail.com>
2018-09-10 13:49:56 +0200

    feature: init android project

---
+++ app/src/main/java/com/example/coen/mad_w2_geo_swipe/MainActivity.kt
@@ -0,0 +1,12 @@
+package com.example.coen.mad_w2_geo_swipe
+
+import android.support.v7.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
